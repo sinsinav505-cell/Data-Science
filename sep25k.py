752f70d5deb48dd18cad6dd41e788361664abc61
Sinsina V <sinsinav505@gmail.com>
2025-09-25 12:40:54 +0530

    Aggregation on Condition

---
+++ sep25k.py
@@ -0,0 +1,18 @@
+# 8. Aggregation on Condition
+# Given a 2D array:
+'''matrix = np.array([[23, 45, 67],
+                   [89, 12, 34],
+                   [56, 78, 90]])'''
+# → Filter all even numbers
+# → Calculate their maximum and minimum
+
+import numpy as np
+
+matrix = np.array([[23, 45, 67],
+                   [89, 12, 34],
+                   [56, 78, 90]])
+
+a=matrix[(matrix%2==0)]
+print(a)
+print(np.max(a))
+print(np.min(a))
