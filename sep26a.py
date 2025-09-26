8ee6f33f88c04ea1424b4c07638e8f957ce90f9a
Sinsina V <sinsinav505@gmail.com>
2025-09-26 11:43:16 +0530

    multiplication table from 1 to 10 using numpy

---
+++ sep26a.py
@@ -0,0 +1,15 @@
+#multiplication table from 1 to 10 using numpy
+
+import numpy as np
+a=np.array([1,2,3,4,5,6,7,8,9,10])
+b=np.array([[1],
+            [2],
+            [3],
+            [4],
+            [5],
+            [6],
+            [7],
+            [8],
+            [9],
+            [10]])
+print(a*b)
