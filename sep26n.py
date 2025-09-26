c0923a213cdd49a5f54e9840eb092a9fbe46b2a0
Sinsina V <sinsinav505@gmail.com>
2025-09-26 12:30:57 +0530

    flatten() – Convert multi-dimensional array into 1D

---
+++ sep26n.py
@@ -0,0 +1,9 @@
+#  flatten() – Convert multi-dimensional array into 1D
+# =======================================
+# Q2: Given: arr2D = np.array([[10, 20], [30, 40], [50, 60]])
+# → Convert this 2D array into a 1D flattened array and print it
+
+import numpy as np
+
+arr2D = np.array([[10, 20], [30, 40], [50, 60]])
+print(arr2D.flatten())
